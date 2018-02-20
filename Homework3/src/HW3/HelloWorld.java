495ea84cbea41907b04bd6ed1e1577903b09062c
ahinojo1 <ahinojo1@mail.depaul.edu>
2018-02-19 18:38:24 -0600

    hello world class works

---
+++ Homework3/src/HW3/HelloWorld.java
@@ -0,0 +1,16 @@
+package HW3;
+
+public class HelloWorld {
+	
+	public static void func1() {
+		System.out.println("I am a function");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		func1();
+
+	}
+
+}
